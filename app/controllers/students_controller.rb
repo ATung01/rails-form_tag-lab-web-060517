cf0e9af1ff7105f2046eec7d66e342b197550f20
github username <github email address>
2017-07-05 18:02:04 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -7,5 +7,19 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  def create
+    @student = Student.last
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    # @student =
+    redirect_to students_path
+  end
+
+  def new
+    @student = Student.new
+    @student.save
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :show, :new, :create]
+  # get '/student/new', to: 'students#new', as: 'new_student' #new_student_path
+  # get '/student/:id', to: 'students#show', as: 'student' #student_path
+  post 'student/:id', to: 'students#create'
+
+
+
+
 end
